4cfaaacf53f1f6a71153eba3befd9d65db06e1ab
Bensoli-Inc <benswambua96@gmail.com>
2024-09-09 23:58:43 +0300

    configuring web.xml

--- src/main/java/com/bensoli/AddServlet.java
+++ src/main/java/com/bensoli/AddServlet.java
@@ -13,6 +13,12 @@ public class AddServlet extends HttpServlet
 	
 	public void service(HttpServletRequest req, HttpServletResponse res)
 	{
+		int i = Integer.parseInt(req.getParameter("num1"));
+		int j = Integer.parseInt(req.getParameter("num2"));
+		
+		int k = i+j;
+		
+		System.out.println("Result is " + k);
 		
 	}
 }
