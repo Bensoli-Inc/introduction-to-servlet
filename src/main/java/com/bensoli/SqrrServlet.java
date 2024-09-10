f99dd9f1242ad4876ba8f2185614a05e853dfb44
Bensoli-Inc <benswambua96@gmail.com>
2024-09-10 13:24:38 +0300

    setting up files for redirect servlet callng

---
+++ src/main/java/com/bensoli/SqrrServlet.java
@@ -0,0 +1,21 @@
+package com.bensoli;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class SqrrServlet extends HttpServlet
+{
+	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException
+	{
+		int k = (int) req.getAttribute("k");
+		
+		
+		PrintWriter out = res.getWriter();
+		out.println("Square of answer is " + k);
+	}
+
+}
