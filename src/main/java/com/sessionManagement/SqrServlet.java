82d077bd53a2c427250b890b5a4631fbd6083a8e
Bensoli-Inc <benswambua96@gmail.com>
2024-09-10 16:48:24 +0300

    session management

---
+++ src/main/java/com/sessionManagement/SqrServlet.java
@@ -0,0 +1,21 @@
+package com.sessionManagement;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class SqrServlet extends HttpServlet
+{
+	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException
+	{
+		int k = Integer.parseInt(req.getParameter("k"));
+		
+		
+		PrintWriter out = res.getWriter();
+		out.println("Square of answer iss " + k);
+	}
+
+}
