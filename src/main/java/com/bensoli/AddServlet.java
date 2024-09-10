5e9d06dd3b790b660ba6a95e905f2902b69590b9
Bensoli-Inc <benswambua96@gmail.com>
2024-09-10 12:08:51 +0300

    calling servlet on servlet

--- src/main/java/com/bensoli/AddServlet.java
+++ src/main/java/com/bensoli/AddServlet.java
@@ -3,6 +3,8 @@ package com.bensoli;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -14,15 +16,21 @@ public class AddServlet extends HttpServlet
 		
 	}
 	
-	public void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException
+	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException
 	{
 		int i = Integer.parseInt(req.getParameter("num1"));
 		int j = Integer.parseInt(req.getParameter("num2"));
 		
 		int k = i+j;
-		
+		k = k*k;
 		PrintWriter out = res.getWriter();
 		out.println("Result is " + k);
 		
+		//using request dispatcher or redirect to call a servlet inside a servlet
+		
+		RequestDispatcher rd = req.getRequestDispatcher("sqr");
+		rd.forward(req, res);
 	}
+	
+	
 }
