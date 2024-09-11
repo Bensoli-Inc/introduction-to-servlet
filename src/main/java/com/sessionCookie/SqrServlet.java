b09639f484be485e8900d7f65d80f7631e6ae5ac
Bensoli-Inc <benswambua96@gmail.com>
2024-09-11 14:16:33 +0300

    csession cookie

--- src/main/java/com/sessionCookie/SqrServlet.java
+++ src/main/java/com/sessionCookie/SqrServlet.java
@@ -1,24 +1,32 @@
-package com.sessionManagement;
+package com.sessionCookie;
 
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
-public class SqrServlet extends HttpServlet
+public class SqrServlet extends HttpServlet 
 {
 	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException
 	{
-		HttpSession session = req.getSession();
-		int k = (int) session.getAttribute("k");
+		int k = 0;
+		Cookie cookies[] = req.getCookies();
 		
+		for(Cookie c: cookies)
+		{
+			if(c.getName().equals("k"))
+				k = Integer.parseInt(c.getValue());
+		}
+		
+				
 		k = k*k;
 		
 		PrintWriter out = res.getWriter();
-		out.println("Square of answer iss " + k);
+		out.println("Result is " + k);
+		
 	}
 
 }
--- src/main/java/com/sessionManagement/AddServlet.java
+++ src/main/java/com/sessionManagement/AddServlet.java
@@ -8,6 +8,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 public class AddServlet extends HttpServlet
 {
@@ -18,9 +19,11 @@ public class AddServlet extends HttpServlet
 		int j = Integer.parseInt(req.getParameter("num2"));
 		
 		int k = i+j;
-		k = k*k;
 		
-		res.sendRedirect("sqra?k="+k);
+		HttpSession session = req.getSession();
+		session.setAttribute("k", k);
+		
+		res.sendRedirect("sqra");
 	}
 	
 	
